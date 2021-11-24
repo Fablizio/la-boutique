8997250189a7f6a0cff783faf277b3f8d170492b
Fablizio <gats200@hotmail.it>
2021-11-24 15:07:54 +0100

    feat aggiunte

--- js/script.js
+++ js/script.js
@@ -1,10 +1,48 @@
-function createProduct(parent, imgUrl, productTitle, textPrice) {
+/** funzione slideshow */
+
+function slideshow() {
+  setTimeout(() => {
+    document.querySelector(".overlay").className = "overlay2";
+
+    setTimeout(() => {
+      document.querySelector(".overlay2").className = "overlay3";
+
+      setTimeout(() => {
+        document.querySelector(".overlay3").className = "overlay";
+      }, 3000);
+    }, 3000);
+  }, 3000);
+}
+
+window.onload = setInterval(function () {
+  slideshow();
+}, 9000);
+
+
+function setCartProductsNum() {
+  cartProductsNum.textContent = `Numero prodotti: ${cartList.length}`;
+}
+
+function createProduct(parent, imgUrl, productTitle, textPrice, idProduct) {
   const product = document.createElement("div");
   product.className = "product";
+  product.setAttribute("id", idProduct);
 
   createImg(product, imgUrl, productTitle);
   createText(product, productTitle, textPrice);
   parent.appendChild(product);
+
+  product.addEventListener("click", (e) => {
+    cartList.push(
+      productsList.find(
+        (product) => parseInt(e.currentTarget.id) === product.id
+      )
+    );
+    setCartProductsNum();
+    alert(`Prodotto aggiunto al carrello, numero prodotti: ${cartList.length}`);
+    // Nel caso in cui volessimo aggiungere una interazione col LocalStorage
+    localStorage.setItem("totCartitems", cartList.length);
+  });
 }
 
 function createImg(parent, imgUrl, productTitle) {
@@ -25,18 +63,15 @@ function createText(parent, productTitle, textPrice) {
   parent.append(title, price);
 }
 
-// fetch("https://fakestoreapi.com/products") // <== importare la lista prodotti in modo remoto
-//   .then((response) => response.json())
-//   .then((data) => {
-//     products = data;
-//     renderProducts();
-//   });
-
-const wrapperProducts = document.querySelector(".wrapper__products");
-
 function renderProducts(listItems) {
   listItems.map((product) => {
-    createProduct(wrapperProducts, product.image, product.title, product.price);
+    createProduct(
+      wrapperProducts,
+      product.image,
+      product.title,
+      product.price,
+      product.id
+    );
   });
 }
 
@@ -44,28 +79,33 @@ function renderProducts(listItems) {
 const getProductsList = async () => {
   const res = await fetch("https://fakestoreapi.com/products");
   const data = await res.json();
+  productsList = data;
+
+  // Nella eventualità di aggiungere una quantità per prodotto
+  // productsList = data.map((product) => {
+  //   product.quantity = 0;
+  //   return product;
+  // });
 
   return renderProducts(data);
 };
 
+let productsList = [];
+const wrapperProducts = document.querySelector(".wrapper__products");
+
+// Parte inerente alla logica del carrello
+let cartList = [];
+
+const localStorageTot = localStorage.getItem("totCartitems");
+const cartBtn = document.querySelector(".cartBtn");
+const cartProductsNum = document.querySelector(".cartProductsNum");
+const clearCartBtn = document.querySelector(".clearCart");
+
+// Flusso generale
+cartProductsNum.textContent = `Numero prodotti: ${localStorageTot}`;
 getProductsList();
 
-/** funzione slideshow */
-
-function slideshow() {
-
-setTimeout(() => {
-  document.querySelector(".overlay").className= "overlay2"
-
-  setTimeout(() => {
-    document.querySelector(".overlay2").className = "overlay3"
-
-    setTimeout(() => {
-      document.querySelector(".overlay3").className = "overlay"
-    },3000);
-  }, 3000);
-}, 3000);
-};
-  
-window.onload = setInterval(function(){slideshow();}, 9000);
-  
+clearCartBtn.addEventListener("click", () => {
+  cartList.length = 0;
+  setCartProductsNum();
+});
